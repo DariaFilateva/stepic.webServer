a7ae3e85f1bd52d717dfb020e9b9752dd0889b9a
Daria Filateva <dariafilatevaprogrammer@gmail.com>
2018-01-27 12:36:01 +0300

    Initial commit

---
+++ Task1/src/main/java/A.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+/**
+ * Created by Dasha on 21.01.2018.
+ */
+class A<T extends List<Number>>{
+
+}
---
+++ Task1/src/main/java/Test.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Dasha on 21.01.2018.
+ */
+public class Test {
+    public static void main(String[] args) {
+        A<List< Long >> a1;
+        A<LinkedList< Long >> a2;
+        A< LinkedList < Object >> a3;
+        A<ArrayList< Number >> a4;
+        A< List < Number >> a5;
+    }
+}
